b68e8695ab5c824e64c0b0d6b817271afb73fe57
Umang <soyumang55@gmail.com>
2024-11-15 18:01:05 +0530

    register route set

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs')
 
-// DB Connection
+// DB CONNECTION
 require('./db/connection')
 
-// Import Files
+// IMPORT FILES
 const Users = require('./models/Users')
 
 const app = express();
@@ -12,6 +13,34 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 // app.use(cors())
+
+// ROUTES
+
+app.post('/api/register',async (req,res,next)=>{
+    try {
+        const {name, email, password} = req.body;
+        if(!name || !email || !password){
+            res.status(400).send("Fill required details")
+        }else{
+            const alreadyExists = await Users.findOne({ email });
+            if(alreadyExists){
+                res.status(400).send("User exists")
+            }
+            else{
+                const newUser = new Users({ name, email })
+                bcryptjs.hash(password, 10, (err, hashedPass)=>{
+                    newUser.set('password', hashedPass);
+                    newUser.save();
+                    next();
+                })
+                return res.status(200).send("User registered successfully")
+            }
+        }
+    } catch (error) {
+        
+    }
+})
+
 
 app.listen(PORT, (req,res)=>{
     console.log(`Listening on port ${PORT}`)
