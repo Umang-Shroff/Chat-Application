384a06779ad21069a5c40fe97d2b5c70b552d65a
Umang <soyumang55@gmail.com>
2024-11-15 23:57:56 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -117,6 +117,7 @@ app.post('/api/message',async (req,res) => {
 app.get('/api/message/:conversationId', async(req,res)=>{
     try {
         const conversationId = req.params.conversationId;
+        if(conversationId === 'new') return res.status(200).json([]);
         const messages = await Messages.find({ conversationId });
         const messageUserData = Promise.all(messages.map(async (message) => {
             const user = await Users.findById(message.senderId);
@@ -131,9 +132,33 @@ app.get('/api/message/:conversationId', async(req,res)=>{
 app.get('/api/users', async (req,res) => {
     try {
         const users = await Users.find();
-        res.status(200).json(users);
+        const usersData = Promise.all(users.map(async (user) => {
+            return { user: { email: user.email, name: user.name }, userId: user._id }
+        }))
+        res.status(200).json(await usersData);
     } catch (error) {
         console.log("Error: ",error)
+    }
+})
+
+app.post('/api/message', async(req,res)=>{
+    try {
+        const { conversationId, senderId, message, receiverId = ''} = req.body;
+        if(!senderId || !message) return res.status(400).send("Please fill all required fields")
+        if(!conversationId && receiverId){
+            const newConversation = new Conversations({ members: [senderId, receiverId] });
+            await newConversation.save();
+            const newMessage = new Messages({ conversationId: newConversation._id, senderId, message });
+            await newMessage.save();
+            return res.status(200).send("Message sent successfully")
+        }else if(!conversationId && !receiverId){
+            return res.status(200).send("Please fill all required fields")
+        }
+        const newMessage = new Messages({ conversationId, senderId, message });
+        await newMessage.save();
+        res.status(200).send("Message sent successfully")
+    } catch (error) {
+        
     }
 })
 
