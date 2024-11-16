f1dd4a1f9a5129b3264bc2233293f00e30710140
Umang <soyumang55@gmail.com>
2024-11-16 19:36:09 +0530

    frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SideBar from './components/sidebar/SideBar';
 import ChatList from './components/chatList/ChatList';
 import Dashboard from './components/dashboard/Dashboard';
+import LoginPage from './pages/login/LoginPage';
+
+const ProtectedRoute = ({ children, auth = false }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || true;
+
+  if(!isLoggedIn && auth){
+    return <Navigate to={'/register'} />
+  }else if(isLoggedIn && ['/register','/login'].includes(window.location.pathname)){
+    return <Navigate to={'/'} />
+  }
+
+  return children
+}
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<HomePage/>}/>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/register' element={<RegisterPage/>}/> */}
+          <Route path='/' element={
+            <ProtectedRoute auth={true}>
+              <Dashboard/>
+            </ProtectedRoute>
+          }/>
+          <Route path='/login' element={
+            <ProtectedRoute>
+              <LoginPage/>
+            </ProtectedRoute>
+          }/>
+          <Route path='/register' element={
+            <ProtectedRoute>
+              <RegisterPage/>
+            </ProtectedRoute>
+          }/>
           <Route path='/chat' element={<SideBar/>}/>
-          <Route path='/list' element={<Dashboard/>}/>
+          {/* <Route path='/list' element={}/> */}
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/pages/login/LoginPage.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    return (
+        <>
+        {/* <Toaster
+            position="bottom-right"
+        /> */}
+        <div className='mt-4 grow flex items-center justify-around'>
+            <div className='mb-32'>
+                <h1 className='text-4xl text-center mb-4'>Login</h1>
+                <form className='max-w-md mx-auto' onSubmit={loginHandle}>
+                  <input type='email' placeholder='your@email.com' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                  <input type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                  <button className='bg-blue-500 text-white'>Login</button>
+                  <div className='text-center py-2 text-gray-500'>Don't have an account yet? <a className='underline text-black' href="/register">Register now</a></div>
+                </form>
+            </div>
+          
+        </div>
+        </>
+      )
+}
+
+export default LoginPage
